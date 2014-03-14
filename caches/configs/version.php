24f1e47186ff21f317204aa53cf014e8cf995d45
hanziacier <hanzi99@126.com>
2014-03-14 11:39:30 +0800

    初始化heymami

---
+++ caches/configs/version.php
@@ -0,0 +1,6 @@
+<?php
+return array(
+'pc_version' => 'V9.1.18',	//phpcms 版本号
+'pc_release' => '20120907',	//phpcms 更新日期
+);
+?>
---
+++ sql.php
@@ -0,0 +1,15 @@
+<?php
+exit;
+$sql ="
+173.252.221.125
+user:hanzhong
+password:ainong.org
+
+www.ainong.org
+create database if not exists 'ainong';
+create user 'ainong'@'%' identified by 'ainong.org.!@#$%^';
+grant all privileges on `ainong`.* to 'ainong'@'%';
+
+admin/ainong7758521
+"
+?>
